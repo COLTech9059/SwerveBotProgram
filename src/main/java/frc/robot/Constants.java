f87716498a381189535d06d5fc5ae0b22ac77c6d
SomnolentStone <jacksonstone0709@gmail.com>
2024-04-24 15:13:44 -0700

    starting constants folder

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,10 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+//Imports
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -13,6 +17,28 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final class portConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+  public static final class driveConstants {
+    //Speed Constraints
+    public static final double maxSpd = 0.0;
+    public static final double maxTurnSpeed = Math.PI * 2; //Radians per second, rotates a full circle in a second at base.
+
+    //Chassis Information
+    public static final double wheelWidth = Units.inchesToMeters(0.0); //Distance between the left and right wheels.
+    public static final double wheelLength = Units.inchesToMeters(0.0); //Distance between the front and back wheels.
+
+    //Each Translation2d represents the wheel in relation to the center of the robot.
+    public static final SwerveDriveKinematics SDK = new SwerveDriveKinematics(
+      new Translation2d(wheelWidth/2, wheelLength/2), //Top right facing forward
+      new Translation2d(-wheelWidth/2, wheelLength/2), //Top left facing forward
+      new Translation2d(wheelWidth/2, -wheelLength/2), //Bottom right facing forward
+      new Translation2d(-wheelWidth/2, -wheelLength/2) //Bottom left facing forward
+    );
+
+  }
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -2,14 +2,14 @@
 // // Open Source Software; you can modify and/or share it under the terms of
 // // the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Command;
 
 // /** An example command that uses an example subsystem. */
-// public class ExampleCommand extends Command {
+// public class DriveCommand extends Command {
 //   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-//   private final ExampleSubsystem m_subsystem;
+//   private final m_Drive m_subsystem;
 
 //   /**
 //    * Creates a new ExampleCommand.
